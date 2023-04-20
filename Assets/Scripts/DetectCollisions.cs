cb5b5b3515e812734228d61d6758bbd3410d8223
jlarata <ariel@cfee.gob.ar>
2023-04-19 22:05:39 -0300

    added DetectCollisions script, to attach to enemies prefabs. this script uses variables from enemycontroller script (thisenemydamage) and detect collision with the playercharacter. if so, calls the losthp function in messicontroller passing ethisenemydamage as variable to lose hp. this function in messi gameobject also manages slider to make the loss visual to the player

---
+++ Assets/Scripts/DetectCollisions.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollisions : MonoBehaviour
+{
+
+  //AudioSource source;
+  //public GameObject gameObjectQueManejeElAudio;
+    public GameObject messi;
+    public MessiController messiController;
+    private float thisEnemyDamage;
+
+    void Start()
+    {
+      messi = GameObject.Find("Messi");
+      messiController = messi.GetComponent<MessiController>();
+      thisEnemyDamage = GetComponent<EnemyController>().enemyDamage;
+      //gameObjectQueManejeElAudio = GameObject.Find("GameObjectQueManejeElAudio");
+      //source = GetComponent<AudioSource>();
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Messi")
+        {
+            messiController.PerderHp(thisEnemyDamage);
+        }
+        
+    }
+    
+    // void OnTriggerEnter(Collider other) {
+      
+    //   if (!(other.gameObject.name == "Animal_Moose_02(Clone)"))
+    //   {
+    //      if (!(this.gameObject.name == "Animal_Moose_02(Clone)")) 
+
+    //     {
+    //     if (!(other.gameObject.name == "Messi"))
+    //     {
+    //       mlemmanager.GetComponent<MlemManager>().Suena();
+
+    //       if (gameObject.GetComponent<AnimalHunger>().currentFedAmount == 0)
+    //       {
+    //         messi.GetComponent<MessiController>().SumarPerritosAlimentados();
+    //       }
+
+    //       if (gameObject.GetComponent<AnimalHunger>().currentFedAmount >=
+    //           gameObject.GetComponent<AnimalHunger>().amountToBeFed)
+    //           {
+    //             gameObject.GetComponent<BoxCollider>().enabled=false;
+    //           }
+          
+    //       //Destroy(gameObject);
+    //       Destroy(other.gameObject);
+    //       gameObject.GetComponent<AnimalHunger>().FeedAnimal(1);
+    //     } else {
+    //     messi.GetComponent<MessiController>().PerderUnaVida();
+    //     }
+    //   }
+      
+    //   //aca va codigo si sos alce 
+    //   if ((other.gameObject.name == "Messi"))
+    //   {
+    //     messi.GetComponent<MessiController>().ALaPuta();
+    //     messi.GetComponent<MessiController>().PerderUnaVida();
+    //   }
+    //   else
+    //   {
+    //   //  Debug.Log("Reno");
+    //   }
+    // }
+  
+    // }
+}
