d33aa1f6704e7cc227e9be1ab21bd43b6d297565
jlarata <ariel@cfee.gob.ar>
2023-09-28 11:34:40 -0300

    first functional button in Market Scene

--- Assets/Scripts/Market/UIButtonsController.cs
+++ Assets/Scripts/Market/UIButtonsController.cs
@@ -25,9 +25,15 @@ public class UIButtonsController : MonoBehaviour
 
     public void InstantiateButtons()
     {
-        GameObject BackButtonInstance = (GameObject)Instantiate(buttonsList[0], new Vector3(661,60,0), new Quaternion(0,0,0,0), GameObject.Find("UI Buttons").transform);
+        GameObject BackButtonInstance = (GameObject)Instantiate(buttonsList[0], new Vector3(0,0,0), new Quaternion(0,0,0,0), GameObject.Find("UI Buttons").transform);
         //BackButtonInstance.transform.SetParent(GameObject.Find("UI Buttons").transform, false);
+        BackButtonInstance.transform.localPosition = new Vector3(-150,0,0);
         Debug.Log("button created");
+        UnityEngine.UI.Button BackButtonInstanceBtn = BackButtonInstance.GetComponent<Button>();
+        Debug.Log("identified component button");
+
+        BackButtonInstanceBtn.onClick.AddListener(BackToTitles);
+        Debug.Log("onclick event added");
 
         //NO PUEDO CARGARLE LA FUNCION AL PREFAB
         //REVISAR EN EL SCRIPT DE TITLES CONTROLLER. SE LE PUEDEN AGREGAR LAS FUNCINOES DESDE EL 
